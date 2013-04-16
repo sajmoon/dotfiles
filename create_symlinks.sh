b62ec81fa2809c23edaa49b693a9e72bd4574329
Simon Ström <simon.strom@gmail.com>
2013-04-16 17:24:28 +0200

    added rbenv, oh my shell etc

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,3 +1,36 @@
-ln -s ~/.dotfiles/vim ~/.vim;
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc;
-ln -s ~/.ditfiles/zshrc ~/.zshrc
+printf "Configuring system\n"
+
+printf "Checking for configfiles\n"
+if [ ! -L ~/.vim ]; then
+  ln -s ~/dotfiles/vim ~/.vim;
+fi
+
+if [ ! -L ~/.vimrc ]; then
+  ln -s ~/dotfiles/vim/vimrc ~/.vimrc;
+fi
+
+if [ ! -L ~/.zshrc ]; then
+  ln -s ~/ditfiles/zshrc ~/.zshrc
+fi
+
+printf "OhMyZsh\n"
+#Install 
+if [ -d ~/.oh-my-zsh ]; then
+  # upgrade 
+  cd ~/.oh-my-zsh
+  git pull
+else
+  # install
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+fi
+
+
+printf "RBENV\n"
+#rbenv
+if [ -d ~/.rbenv ]; then
+  cd ~/.rbenv
+  git pull
+else
+  git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
