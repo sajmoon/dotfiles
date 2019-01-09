e2966d616eab6abf484643747dc24af2d5e56540
Simon Strom <simon.strom@gmail.com>
2019-01-09 11:03:28 +0100

    fish: set some variables. Lang and editor

--- variables.fish
+++ variables.fish
@@ -1,5 +1,9 @@
 # Local variables that is local to this machine only
 
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+set -x EDITOR nvim
+
 if test -e ~/.variables_local.fish
   source ~/.variables_local.fish
 end
