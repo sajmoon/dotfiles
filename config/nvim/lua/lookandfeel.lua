930d4b5e3163510e80b883eaf17969dedbb9292b
Simon Ström <simon@fullview.io>
2025-11-21 13:18:48 +0100

    vim: disable icons since we dont have the font

--- config/nvim/lua/lookandfeel.lua
+++ config/nvim/lua/lookandfeel.lua
@@ -4,7 +4,13 @@ vim.o.termguicolors = true
 require('lualine').setup({
   options = {
     theme = 'gruvbox',
-    icons_enabled = true,
+    icons_enabled = false,
   }
+})
+
+-- Disable devicons globally
+require('nvim-web-devicons').setup({
+  override = {},
+  default = true,
 })
 require('colorizer').setup()
