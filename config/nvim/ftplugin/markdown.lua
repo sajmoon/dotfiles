396e220d297a6971b9e9796173ce935b029663a8
Simon Ström <simon@fullview.io>
2026-04-21 16:42:55 +0200

    vim: exclude urls and code from spell checking

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -10,6 +10,13 @@ vim.opt_local.textwidth = 80
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 
--- Spell check for markdown
+-- Spell check for markdown, but not in URLs or code blocks
 vim.opt_local.spell = true
 vim.opt_local.spelllang = 'en_us'
+
+-- Exclude URLs, code blocks, and inline code from spell checking
+vim.cmd([[
+  syn match markdownUrl `\%(https\?\|ftp\):\/\/[[:alnum:]%\/_#.-]*` contains=@NoSpell
+  syn region markdownCode matchgroup=markdownCodeDelimiter start="`" end="`" contains=@NoSpell
+  syn region markdownCodeBlock start="^\s*```" end="^\s*```" contains=@NoSpell
+]])
